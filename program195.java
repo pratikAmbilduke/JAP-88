d141fd5a03b474815f179ece44d169e55db9adbd
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-03-31 00:17:46 +0530

    Day1 pattern program

--- program195.java
+++ program195.java
@@ -3,18 +3,21 @@ class program195
 	public static void main(String args[])
 	{
 		int n=5;
-		for(int k=1; k<=n; k++)
+		for(int i=1; i<=(n*2)-1; i++)
 		{
-			for(int i=1; i<=n; i++)//rows
-			{
-				for(int j=1; j<=n; j++)//column
-				{
-					System.out.print("*");
-				}
+			if(i<=n)
+				for(int j=1; j<=i; j++)
+					if(j%2!=0)
+						System.out.print((char)(j+64));
+					else
+						System.out.print((char)(j+96));
+			else
+				for(int j=1; j<=(n*2)-i; j++)
+					if(j%2!=0)
+						System.out.print((char)(j+64));
+					else
+						System.out.print((char)(j+96));
 				System.out.println();
-			}
-			System.out.print("\t");
 		}
-		
 	}
 }
