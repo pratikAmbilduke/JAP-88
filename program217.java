d6a02c659e8a2dc97c131e064c7d072b6c8997c3
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-04-01 13:57:13 +0530

    Day4 Two traingle Number Pattern

---
+++ program217.java
@@ -0,0 +1,26 @@
+class program217
+{
+	public static void main(String args[])
+	{
+		int n=5;
+		for(int i=1; i<=(n*2)-1; i++)
+		{
+			if(i<=n)
+			{
+				for(int j=i; j<n; j++)
+					System.out.print(" ");
+				for(int j=1; j<=i; j++)
+					System.out.print(j);
+			}
+			else
+			{
+				for(int j=i; j>n; j--)
+					System.out.print(" ");
+				for(int j=1; j<=(n*2)-i; j++)
+					System.out.print(j);
+
+			}
+			System.out.println();
+		}
+	}
+}
