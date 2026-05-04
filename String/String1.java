7d13507104e9dbba8e75119cb20afdc889b12b3a
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-05-04 13:09:19 +0530

    WAP to shift firt chracter to last(2nd Method)

---
+++ String/String1.java
@@ -0,0 +1,14 @@
+class String1
+{
+	public static void main(String args[])
+	{
+		String s1= "India";
+		StringBuilder sb= new StringBuilder();
+		for(int i=1; i<s1.length(); i++)
+			sb.append(s1.charAt(i));
+			sb.append(s1.charAt(0));
+		System.out.println(sb);
+	}
+}
+
+	
