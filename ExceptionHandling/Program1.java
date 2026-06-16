2f0c02a4a9992b1bd220997555256c0ef4b5963c
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-06-16 11:08:57 +0530

    Finallt block added

--- ExceptionHandling/Program1.java
+++ ExceptionHandling/Program1.java
@@ -2,14 +2,22 @@ class Program1
 {
 	int divide(int a, int b)
 	{
+		System.out.println("Program Started");
 		try{
 		return a/b;
 		}
+
 		catch(ArithmeticException e){
 			System.out.println(e);
 		return 0;	
 		}
+		
+		finally{
+			System.out.println("Finally Block Exexuted");
+		}
+
 	}
+	
 }
 
 class MainClass
@@ -17,6 +25,6 @@ class MainClass
 	public static void main(String args[])
 	{
 		Program1 p= new Program1();
-		System.out.println(p.divide(10,0));
+		System.out.println(p.divide(10,10));
 	}
 }
