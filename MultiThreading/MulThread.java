18d0d4dfe98320b3df6717fba85d472b5f27222a
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-06-23 11:16:08 +0530

    Updated getId and getName methof of Thread

--- MultiThreading/MulThread.java
+++ MultiThreading/MulThread.java
@@ -3,6 +3,8 @@ class MulThread extends Thread
 	public void run()
 	{
 		System.out.println("Run method called");
+		System.out.println("Thread Name: "+getName());
+
 	}
 }
 
@@ -11,7 +13,9 @@ class MainClass
 	public static void main(String args[])
 	{
 		MulThread t= new MulThread();
+		MulThread t1= new MulThread();
 		t.start();
+		t1.start();
 		System.out.println("Thread ID: "+t.getId());
 	}
 }
