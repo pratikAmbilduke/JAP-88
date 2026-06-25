6888fe2d51422a548722fe9f3d0c0b909c395bd7
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-06-25 16:04:37 +0530

    Basic Collection method i.e. add()

---
+++ Collection Framework/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Collection<String> c =
+            new ArrayList<>();
+
+        c.add("Java");
+        c.add("Python");
+
+        System.out.println(c);
+    }
+}
