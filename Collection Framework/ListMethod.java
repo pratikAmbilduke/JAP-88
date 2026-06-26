6b3282b17985d5ddb92b4fc058089900f39df7b0
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-06-26 13:29:41 +0530

    add() method of List

---
+++ Collection Framework/ListMethod.java
@@ -0,0 +1,14 @@
+import java.util.*;	
+
+class ListMethod
+{
+	public static void main(String args[])
+	{
+		List<String> list= new ArrayList<>();
+		list.add("java");
+		list.add("python");
+		list.add("java");
+
+		System.out.println(list);
+	}
+}
