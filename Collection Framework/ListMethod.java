b1b9abcc36bccaf8d72e6326d9b3277af5fd0fb0
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-06-26 14:26:30 +0530

    size() method of List introduced

--- Collection Framework/ListMethod.java
+++ Collection Framework/ListMethod.java
@@ -15,5 +15,6 @@ class ListMethod
 		System.out.println(list.indexOf("java"));
 		System.out.println(list.lastIndexOf("Modi"));
 		System.out.println(list.contains("Modi"));
+		System.out.println("Size of an List:"+list.size());
 	}
 }
