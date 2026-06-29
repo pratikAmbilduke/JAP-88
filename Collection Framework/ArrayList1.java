fe33664d5160268551efed542d5f608d3cc35538
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-06-29 11:08:21 +0530

    ArrayList

---
+++ Collection Framework/ArrayList1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class ArrayList1
+{
+	public static void main(String args[])
+	{
+		ArrayList<String> list= new ArrayList<>();
+		
+		list.add("java");
+		
+		System.out.println(list);
+	}
+}
