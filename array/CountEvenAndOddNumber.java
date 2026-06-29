7891f3e1ea2081ad94aae414942c600ff589077b
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-06-29 14:31:48 +0530

    Even and odd number

---
+++ array/CountEvenAndOddNumber.java
@@ -0,0 +1,19 @@
+class CountEvenAndOddNumber
+{
+	public static void main(String args[])	
+	{
+		int arr[]= {1,2,3,4,5,6,7,9};
+		int even= 0; 
+		int odd= 0;
+		for(int i=0; i<arr.length; i++)
+		{
+			if(arr[i]%2==0)
+				even++;
+			else
+				odd++;
+		}
+		System.out.println("Even:"+even);
+		System.out.println("odd:"+odd);
+
+	}
+}
