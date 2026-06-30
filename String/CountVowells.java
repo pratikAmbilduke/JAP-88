aa7e3bd9fc8820fa08907e7c9dfd2588fcd69acb
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-06-30 13:14:15 +0530

    Count Vowells

---
+++ String/CountVowells.java
@@ -0,0 +1,23 @@
+/*
+	WAP to count Vowells
+*/
+
+
+class CountVowells
+{
+	public static void main(String args[])
+	{
+		String str= "programming";
+		int count= 0;
+		
+		for(int i=0; i<str.length(); i++)
+		{
+			char ch= Character.toLowerCase(str.charAt(i));
+			if(ch== 'a' || ch== 'e' || ch== 'i' || ch== 'o' || ch== 'u')
+			{
+				count++;
+			}
+		}
+		System.out.println("Number of vowellls:"+count);
+	}
+}
