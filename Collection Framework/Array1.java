9645da2d076f6c8ff88f7694215626e5270a04d5
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-07-01 11:47:04 +0530

    Vector

---
+++ Collection Framework/Array1.java
@@ -0,0 +1,9 @@
+class Array1
+{
+	public static void main(String args[])
+	{
+		int arr[5]= new arr[1,2,3,4,5];
+		for(int i=1; i<=arr.length(); i++)
+			System.out.println(arr);
+	}
+}
---
+++ Collection Framework/VectorClass.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class VectorClass
+{
+	public static void main(String args[])
+	{
+		List<String> l= new Vector<>();	
+		
+		l.add("pratik");
+		l.add("golu");
+		l.add("golu");
+		System.out.println(l);	
+	}
+}
