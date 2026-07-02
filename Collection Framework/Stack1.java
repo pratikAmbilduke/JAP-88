9d4dc327ae06d787463b3d35f2619a6bd40c4963
pratikAmbilduke <pratikambilduke09@gmail.com>
2026-07-02 09:57:08 +0530

    search() method

--- Collection Framework/Stack1.java
+++ Collection Framework/Stack1.java
@@ -11,5 +11,10 @@ class Stack1
 		s.pop();
 		System.out.println("After pop Operation:"+s);
 		System.out.println(s.empty());
+		s.push(30);
+		System.out.println(s);
+		System.out.println("Top element of the Stack:"+s.peek());
+		System.out.println(s);
+		System.out.println("search():"+s.search(30));
 	}
 }
